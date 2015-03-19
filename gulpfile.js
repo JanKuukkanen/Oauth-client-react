18c687a2dc2ffdfc161734e353dcce9751107723
Taneli Hartikainen <taneli.hartikainen@jamk.fi>
2015-03-19 08:42:03 +0200

    Clean up Gulpfile. Make subsequent Browserify builds about 10x faster.

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,23 @@
 
 var gulp       = require('gulp');
 var sass       = require('gulp-sass');
-var react      = require('gulp-react');
 var mocha      = require('gulp-mocha');
 var jshint     = require('gulp-jshint');
 var server     = require('gulp-webserver');
 var source     = require('vinyl-source-stream');
 var envify     = require('envify');
-var reactify   = require('reactify');
+var babelify   = require('babelify');
+var watchify   = require('watchify');
 var browserify = require('browserify');
 
+// Setup our Browserify transform, so that it supports watchify.
+var bundler = watchify(browserify('./src/scripts/app.js', watchify.args))
+	.transform(envify)
+	.transform(babelify);
+
+/**
+ * Unit tests.
+ */
 gulp.task('test', function() {
 	return gulp.src('./test/**/*.js')
 		.pipe(mocha({
@@ -21,13 +29,28 @@ gulp.task('test', function() {
 		}))
 });
 
-gulp.task('jshint', function() {
+/**
+ * Static code analysis.
+ */
+gulp.task('lint', function() {
 	return gulp.src('./src/scripts/**/*')
 		.pipe(jshint('.jshintrc'))
 		.pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('sass', function() {
+/**
+ * Build the JavaScript source.
+ */
+gulp.task('build-js', function() {
+	return bundler.bundle()
+		.pipe(source('app.js'))
+		.pipe(gulp.dest('dist'));
+});
+
+/**
+ * Build the CSS source.
+ */
+gulp.task('build-css', function() {
 	return gulp.src('src/sass/app.sass')
 		.pipe(sass({
 			indentedSyntax:  true,
@@ -36,22 +59,23 @@ gulp.task('sass', function() {
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('static', function() {
+/**
+ * Build the various assets, such as images, etc...
+ */
+gulp.task('build-assets', function() {
 	return gulp.src('./src/assets/**/*')
 		.pipe(gulp.dest('./dist/assets/'));
 });
 
-gulp.task('browserify', function() {
-	return browserify('./src/scripts/app.js')
-		.transform(reactify)
-		.transform(envify)
-		.bundle()
-		.pipe(source('app.js')).pipe(gulp.dest('dist'));
-});
+/**
+ * Build everything!
+ */
+gulp.task('build', ['build-assets', 'build-css', 'build-js']);
 
-gulp.task('build', ['sass', 'static', 'browserify']);
-
-gulp.task('serve', ['jshint', 'build'], function() {
+/**
+ * Serve the static content and run a livereload server.
+ */
+gulp.task('serve', ['build'], function() {
 	return gulp.src('.')
 		.pipe(server({
 			host:       process.env.HOSTNAME || '0.0.0.0',
@@ -60,8 +84,12 @@ gulp.task('serve', ['jshint', 'build'], function() {
 		}));
 });
 
+/**
+ * Keep track of the source files and rebuild as necessary.
+ */
 gulp.task('default', ['serve'], function() {
-	gulp.watch('./src/sass/**/*.sass',   ['sass']);
-	gulp.watch('./src/scripts/**/*.js',  ['browserify', 'jshint']);
-	gulp.watch('./src/scripts/**/*.jsx', ['browserify', 'jshint']);
+	gulp.watch('./src/sass/**/*.sass',   ['build-css']);
+	gulp.watch('./src/assets/**/*',      ['build-assets']);
+	gulp.watch('./src/scripts/**/*.js',  ['lint', 'build-js']);
+	gulp.watch('./src/scripts/**/*.jsx', ['lint', 'build-js']);
 });
