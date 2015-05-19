7b1f4cfcb70d1189c5fb92282d44a1dba61eb370
matti <melonmanchan@gmail.com>
2015-05-19 09:43:34 +0300

    Remove export and share features from board editing dialog

--- src/scripts/components/dialog/edit-board.js
+++ src/scripts/components/dialog/edit-board.js
@@ -4,12 +4,12 @@ import Board       from '../../models/board';
 import BoardAction from '../../actions/board';
 
 import Dialog           from '../../components/dialog';
-import BoardExporter    from '../../components/board-exporter';
 import BackgroundSelect from '../../components/background-select';
 
 /**
  *
  */
+	
 export default React.createClass({
 	mixins: [ React.addons.PureRenderMixin, React.addons.LinkedStateMixin ],
 
@@ -40,32 +40,7 @@ export default React.createClass({
 		return this.props.onDismiss();
 	},
 
-	hide() {
-		BoardAction.revokeAccessCode({ id: this.props.board.id });
-	},
-
-	share() {
-		BoardAction.generateAccessCode({ id: this.props.board.id });
-	},
-
 	render() {
-		let id   = this.props.board.id;
-		let code = this.props.board.accessCode;
-
-		let sharedURL = code !== null && code.length > 0
-			? location.host + '/boards/' + id + '/access/' + code + ''
-			: '';
-
-		let shareButtonClass = sharedURL.length > 0 ? 'neutral' : 'secondary';
-		let shareButtonClick = sharedURL.length > 0 ? this.hide : this.share;
-
-		let shareButton = (
-			<button className={`btn-${shareButtonClass}`}
-					onClick={shareButtonClick}>
-				{ sharedURL.length > 0 ? 'Hide' : 'Share' }
-			</button>
-		);
-
 		return (
 			<Dialog className="dialog-edit-board"
 					onDismiss={this.props.onDismiss}>
@@ -77,15 +52,6 @@ export default React.createClass({
 					<label htmlFor="board-name">Board Name</label>
 					<input name="board-name" placeholder="Board Name"
 						valueLink={this.linkState('name')} autoFocus={true} />
-
-					<label htmlFor="board-share">Shared URL</label>
-					<section className="input-group">
-						<input name="board-share" placeholder="Shared URL"
-							readOnly={true} value={sharedURL} tabIndex={-1}/>
-						{shareButton}
-					</section>
-
-					<BoardExporter boardID={id} />
 
 					<BackgroundSelect background={this.linkState('background')}
 						customBackground={this.linkState('customBackground')} />
