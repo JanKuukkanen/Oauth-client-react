1c049ff237443a2e988bb271f2caf7e2f185a7e4
kuutamol <lasse.jokine@gmail.com>
2015-06-01 08:51:16 +0300

    Added sass styling to info textboxes.

--- src/scripts/components/dialog/view-info.js
+++ src/scripts/components/dialog/view-info.js
@@ -8,7 +8,6 @@ import TextBoxes	from './text-box';
  */
 export default React.createClass({
 	mixins: [ Carousel.ControllerMixin ],
-
 	getInitialState(){
 		return {currentSlide: null}
 	},
@@ -16,20 +15,16 @@ export default React.createClass({
 	componentDidMount() {
 		this.el = document.getElementById('application');
 		this.el.className = 'info-view-active';
-
 		this.avatar = document.getElementById('avatar');
-
 	},
 
 	componentWillUnmount() {
 		this.el.className = '';
-
 	},
 
 	componentDidUpdate(){
-		this.state.currentSlide=this.state.carousels.carousel.state.currentSlide;
-		this.el.className = `info-view-active slide-${this.state.currentSlide}`;
-		
+		this.el.className = 
+		`info-view-active slide-${this.state.carousels.carousel.state.currentSlide}`;
 	},
 
 	render() {
@@ -38,9 +33,18 @@ export default React.createClass({
 		: this.state.carousels.carousel.state.currentSlide;
 
 		let boxes = {
-			eka : 
-				[{ title: 'Back', content: 'Here is the back button', class: 'pos-back' },
-			 	{ title: 'Edit Board', 	content: 'Edit board', class:'pos-edit' }],
+			eka :[ 
+				{ title: 'Back', content: 'Return to workspace', class: 'pos-back' },
+			 	{ title: 'Edit Board', 	content: 'Edit board', class:'pos-edit' },
+			 	{ title: 'Share Board', 	content: 'Share board', class:'pos-share' },
+			 	{ title: 'Export Board', 	content: 'Export board yo', class:'pos-export' },
+			 	{ title: 'Magnet', 	content: 'Mag', class:'pos-magnet' },
+			 	{ title: 'Gridify', 	content: 'Toggle board overview and navigate', class:'pos-minimap' },
+			 	{ title: 'Profile', 	content: 'Edit your profile man', class:'pos-profile' },
+			 	{ title: 'Localization', 	content: 'legalize0 man', class:'pos-localization' },
+			 	{ title: 'Feedback', 	content: 'Send feedback', class:'pos-feedback' },
+			 	{ title: 'Logout', 	content: 'Mag bigmäg yo män', class:'pos-logout' }
+			 	],
 		 	toka :
 				[{ title: 'Back', content: 'Here is the back button', class: 'pos-back' },
 			 	{ title: 'Edit Board', 	content: 'Edit board', class:'pos-edit' }]
